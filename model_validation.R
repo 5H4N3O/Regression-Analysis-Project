5ba522ba4e3f3ba9fb4912b79a9032ce7b763f37
BigAndGreedy <rehbe059@umn.edu>
2025-04-16 16:14:27 -0500

    working on model validation

--- model_validation.R
+++ model_validation.R
@@ -24,3 +24,22 @@ shapiro.test(model_A2$residuals) # P small
 
 # Cannot fix normality assumption, apply other remedials to model and use 
 # bootstrap PIs and CIs 
+
+library(car) # for ncvTest (Breusch-pagan)
+
+ncvTest(model_A2) # 0.0011912, heteroskedasticity present
+
+### Do WLS regression and see how that works out
+
+weight_mod = lm(abs(model_A2$residuals) ~ model_A2$fitted.values)
+sigma_sq = weight_mod$residuals^2
+w.i. = 1/sigma_sq
+
+model_weighted = lm(data = Admission, formula = I(Y^2) ~  GRE + TOEFL + LOR + CGPA + Research, weights = w.i.)
+model_weighted |> summary()
+plot(model_weighted)
+
+shapiro.test(model_weighted$residuals) # still nonormal 
+
+
+
